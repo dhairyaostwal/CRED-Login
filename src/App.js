b2e63f7e441c90d7f071e91caa83775ef2c9187a
Dhairya <dhairya.ostwal@gmail.com>
2021-05-02 08:51:47 +0530

    Updated links

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
       <div className="form--card">
         <div style={{ color: "#EBB997", fontSize: "1.7rem" }}>welcome to CRED club</div>
         <div>
-          <img src={gitHubLogo} className="form--icon" href="https://github.com/dhairyaostwal" />
-          <img src={linkedinLogo} className="form--icon" href="https://www.linkedin.com/in/dhairyaostwal/" />
-          <img src={mediumLogo} className="form--icon" href="https://medium.com/@dhairyaostwal" />
+          <a href="https://github.com/dhairyaostwal" ><img src={gitHubLogo} className="form--icon" /></a>
+          <a href="https://linkedin.com/dhairyaostwal" ><img src={linkedinLogo} className="form--icon" /></a>
+          <a href="https://medium.com/@dhairyaostwal"><img src={mediumLogo} className="form--icon" /></a>
         </div>
         <p><input placeholder="email" type="text" className="form--input" /></p>
         <p><input placeholder="password" type="password" className="form--input" /></p>
-        <p><button className="btn" href="https://github.com/dhairyaostwal/CRED-Login">Sign In</button></p>
+        <p><a href="https://github.com/dhairyaostwal/CRED-Login"><button className="btn">Sign In</button></a></p>
 
         <div>New here? Create account<br />Forgot your password?</div>
       </div>
-    </div>
+    </div >
   );
 }
 
