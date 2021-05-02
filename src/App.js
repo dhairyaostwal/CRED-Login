8a667ed2a157d6acc31a93bf381f1c81dca05f00
Dhairya <dhairya.ostwal@gmail.com>
2021-05-02 07:54:13 +0530

    Added links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import gitHubLogo from './icons/github.png';
 import mediumLogo from './icons/medium.png';
@@ -11,13 +11,13 @@ function App() {
       <div className="form--card">
         <div style={{ color: "#EBB997", fontSize: "1.7rem" }}>welcome to CRED club</div>
         <div>
-          <img src={gitHubLogo} className="form--icon" />
-          <img src={linkedinLogo} className="form--icon" />
-          <img src={mediumLogo} className="form--icon" />
+          <img src={gitHubLogo} className="form--icon" href="https://github.com/dhairyaostwal" />
+          <img src={linkedinLogo} className="form--icon" href="https://www.linkedin.com/in/dhairyaostwal/" />
+          <img src={mediumLogo} className="form--icon" href="https://medium.com/@dhairyaostwal" />
         </div>
         <p><input placeholder="email" type="text" className="form--input" /></p>
         <p><input placeholder="password" type="password" className="form--input" /></p>
-        <p><button className="btn">Sign In</button></p>
+        <p><button className="btn" href="https://github.com/dhairyaostwal/CRED-Login">Sign In</button></p>
 
         <div>New here? Create account<br />Forgot your password?</div>
       </div>
